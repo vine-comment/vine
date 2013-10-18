f3d30524676872dcad64eecca4a29e9b46284d10
GeekAn <geekan@foxmail.com>
2013-10-18 22:44:18 +0800

    update

--- browser/chrome-extension/god-alike-comment/shentucao.js
+++ browser/chrome-extension/god-alike-comment/shentucao.js
@@ -1,26 +1,3 @@
-
-(function() {
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName("head")[0].appendChild(script);
-
-    // Poll for jQuery to come into existance
-    var checkReady = function(callback) {
-        if (window.jQuery) {
-            callback(jQuery);
-        }
-        else {
-            window.setTimeout(function() { checkReady(callback); }, 100);
-        }
-    };
-
-    // Start polling...
-    checkReady(function($) {
-        // Use $ here...
-		$('body').append('<div id="test"></div>');
-		$('#test').load("http://www.anwcl.com:8000/static/test/iframe_test.html");
-    });
-})();
-
+var jQuery_anwcl = jQuery.noConflict();
+jQuery_anwcl('body').append('<div id="test"></div>');
+jQuery_anwcl('#test').load("http://www.anwcl.com:8000/static/test/iframe_test.html");
