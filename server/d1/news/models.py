f396b06f853d6605e5f801676cd4469a1b6b4575
GeekAn <geekan@foxmail.com>
2013-10-28 19:51:54 +0800

    update

---
+++ server/d1/news/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __unicode__(self):
+        return self.full_name
+
+class Article(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter)
+
+    def __unicode__(self):
+        return self.headline
