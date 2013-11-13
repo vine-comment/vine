594b612501c255a49dedd6fe9ecccb8f169cc3bc
GeekAn <geekan@foxmail.com>
2013-11-13 22:49:48 +0800

    update

---
+++ server/d1/testapp/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.views.generic import View, TemplateView, RedirectView
+
+class MyView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
+    
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.views.generic.base import RedirectView
+
+from articles.models import Article
+
+class ArticleCounterRedirectView(RedirectView):
+
+    permanent = False
+    query_string = True
+
+    def get_redirect_url(self, pk):
+        article = get_object_or_404(Article, pk=pk)
+        article.update_counter()
+        return reverse('product_detail', args=(pk,))
