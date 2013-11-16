1b86a7b217f6dcb53ee49ade36d2ae2acd5a0264
GeekAn <geekan@foxmail.com>
2013-11-16 21:24:11 +0800

    update

---
+++ server/d1/d1/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea)
+
+    def send_email(self):
+        # send email using the self.cleaned_data dictionary
+        pass
