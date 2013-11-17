388f6a92482400c334f0257c4dac07017de6ec8e
GeekAn <geekan@foxmail.com>
2013-11-17 15:38:18 +0800

    update

---
+++ server/d1/comments/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from comments.models import Comment
+
+admin.site.register(Comment)
--- server/d1/d1/urls.py
+++ server/d1/d1/urls.py
@@ -34,7 +34,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += patterns('',
