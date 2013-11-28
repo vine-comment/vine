865974f4fb0652d99cc0cf9ead2dd6f833537a58
GeekAn <geekan@foxmail.com>
2013-11-29 00:27:35 +0800

    update

--- server/d1/d1/urls.py
+++ server/d1/d1/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from d1.views import *
-from testapp.view import *
+from testapp.views import *
 from comments.views import *
 
 from django.conf.urls import *
