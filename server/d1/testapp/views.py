84ed8c500f3d49c8fdbadfb36cf87f191872227f
GeekAn <geekan@foxmail.com>
2013-12-07 20:58:41 +0800

    magic!

--- server/d1/testapp/views.py
+++ server/d1/testapp/views.py
@@ -10,6 +10,19 @@ from news.models import Article
 from django.utils import timezone
 from django.http import *
 
+#coding:utf-8
+from django.http import *
+#from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt 
+from time import gmtime, strftime
+from django.shortcuts import render, render_to_response
+from django.views.generic import *
+from urlparse import urlparse
+from django.core.paginator import Paginator
+import datetime
+from django.utils.timezone import utc
+import base64
+
 from testapp.models import *
 
 class MyView(TemplateView):
@@ -197,6 +210,82 @@ class CommentListView(ListView):
 #         context = super(CommentListView, self).get_context_data(**kwargs)
 #         return context
 '''
+    
+def leave_comment(comment_str, index_url):
+    #len(messageBoard), 
+    
+    comment_tuple = [strftime("%Y-%m-%d %H:%M:%S", gmtime()), comment_str]
+    messageBoard.append(comment_tuple)
+
+    msgboards[index_url] = msgboards.get(index_url, [])
+    msgboards[index_url].append(comment_tuple)
+    
+def write_comment_board(request, refer_url):
+    comment = request.POST.get('comment', 'Empty Comment')
+    leave_comment(comment, refer_url)
+
+    return comment
+
+def get_comment_board_template(request, refer_url):
+    req_page = request.GET.get('page', None)
+    if req_page is None or req_page is '':
+        req_page = 1
+
+    COMMENT_PER_PAGE = 5
+    end_comment = (-req_page)*COMMENT_PER_PAGE
+    start_comment = (1-req_page)*COMMENT_PER_PAGE
+    if start_comment is 0:
+        start_comment = None
+    
+    if refer_url is None:
+        msgboard = messageBoard
+    else:
+        msgboard = msgboards.get(refer_url)
+        if msgboard is None:
+            #FIXME invalid case
+            pass
+    
+    #获得最后COMMENT_PER_PAGE条
+    to_show_messages = reversed(msgboard[end_comment:start_comment])
+    #如果多余COMMENT_PER_PAGE条，翻页
+    page_count = len(msgboard) / COMMENT_PER_PAGE + 1
+    
+    #render html here
+    template_name = "comments/comment_board_get.html"
+    return render(request, template_name, {
+        'messages': to_show_messages,
+        'n_messages': len(msgboard),
+        'message_per_page': COMMENT_PER_PAGE,
+        'refer_url': refer_url,
+        'n_page': page_count + 1,
+    })
+
+def debug_comment(refer_url, netloc):
+    return leave_comment(refer_url, netloc)
+
+@csrf_exempt 
+def comment_board(request, refer_url_b64 = None):
+    '''
+    主要程序入口，读取URL中的BASE64字符串并打印到board上，并解析POST/GET的参数，
+    进行相应的动作。
+    '''
+    #不管是否是post，都先解析字符串，把refer_url打到comment里
+    if refer_url_b64:
+        refer_url = base64.b64decode(refer_url_b64)
+        netloc = urlparse(refer_url).netloc
+        debug_comment(refer_url, netloc)
+    #如果是POST，那么写comment_board
+    if request.method == 'POST':
+        write_comment_board(request, netloc)
+    #然后返回新的HTML，刷新掉老的。这里用jquery动态加载回复。
+    res = HttpResponse(get_comment_board_template(request, netloc))
+    res['Access-Control-Allow-Origin'] = '*'
+    return res
+
+msgboards = {}    #区分URL的msgboards
+messageBoard = [] #只有一个的全局messageBoard
+cursor = 0
+    
 msgboards = {}    #区分URL的msgboards
 messageBoard = [] #只有一个的全局messageBoard
 cursor = 0
