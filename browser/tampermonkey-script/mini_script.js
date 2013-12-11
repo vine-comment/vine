1b8838b5433fc40e4109149d5e35662bbe9aca0d
GeekAn <geekan@foxmail.com>
2013-12-12 00:16:25 +0800

    update tampermonkey scripts locations

--- browser/tampermonkey-script/mini_script.js
+++ browser/tampermonkey-script/mini_script.js
@@ -1,21 +1,14 @@
 // ==UserScript==
 // @name       try-to-comment
-// @namespace  http://use.i.E.your.homepage/
-// @version    0.1
-// @description  enter something useful
-// @match      http://www.anwcl.com
+// @namespace  http://www.anwcl.com/
+// @version    0.2
+// @description  3rd party comment plugin
+// @match      http://www.anwcl.com/contact.html
+// @exclude    http://www.anwcl.com:8000/*
 // @copyright  2012+, You
 // @require    http://code.jquery.com/jquery-1.8.2.min.js
 // @grant      unsafeWindow
 // ==/UserScript==
 
-$('body').append('<div id="test"></div>');
-$('#test').load("http://www.anwcl.com/test/test_only_div.html");
-
-// Use any event to append the code
-$(document).ready(function() 
-{
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + 'www.anwcl.com/test/commentBoard.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-});
+$('body').append('<div id="vine_comment_iframe"></div>');
+$('#vine_comment_iframe').load("http://www.anwcl.com:8000/iframe/" + btoa(document.location));
