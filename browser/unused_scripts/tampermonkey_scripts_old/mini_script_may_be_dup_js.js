cf004cb9e03e7c4320e0c0ed7ade5b6bc952c5d1
GeekAn <geekan@foxmail.com>
2013-12-12 02:07:05 +0800

    update browser folder README and locations

---
+++ browser/unused_scripts/tampermonkey_scripts_old/mini_script_may_be_dup_js.js
@@ -0,0 +1,7 @@
+// Use any event to append the code
+$(document).ready(function() 
+{
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + 'www.anwcl.com:8000/static/test/floatCommentBoard.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+});
