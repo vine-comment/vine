a718836f29d6677ba29772059c9636a41567de01
GeekAn <geekan@foxmail.com>
2013-12-12 23:46:01 +0800

    update matrix template, now 8000 homepage shows matrix

--- server/d1/d1/urls.py
+++ server/d1/d1/urls.py
@@ -9,7 +9,7 @@ from vine_comment.views import *
 urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='matrix/matrix.html'), name='index'),
     url(r'comment/(?P<url_b64>.*?)/?$', CommentView.as_view(), name='comment'),
 )
 
