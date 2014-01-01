2aa234a2a53c45ed322440037c9f4bce56bea113
GeekAn <geekan@foxmail.com>
2014-01-01 22:20:43 +0800

    remove useless resource

--- browser/chrome-extension/god-alike-comment/shentucao.js
+++
@@ -1,3 +0,0 @@
-var jQuery_anwcl = jQuery.noConflict();
-jQuery_anwcl('body').append('<div id="test"></div>');
-jQuery_anwcl('#test').load("http://www.anwcl.com:8000/static/test/iframe_test.html");
---
+++ browser/user-extension/chrome-extension/god-alike-comment/shentucao.js
@@ -0,0 +1,3 @@
+var jQuery_anwcl = jQuery.noConflict();
+jQuery_anwcl('body').append('<div id="test"></div>');
+jQuery_anwcl('#test').load("http://www.anwcl.com:8000/static/test/iframe_test.html");
