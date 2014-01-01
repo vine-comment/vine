700e0c96ce30d6f1876defc8a21e2a4a8b1217dc
GeekAn <geekan@foxmail.com>
2014-01-01 21:35:02 +0800

    remove useless app: threaded-comments

--- server/django-threaded-comments/examples/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'text',)
-
-admin.site.register(Message, MessageAdmin)
