c8cc1542fa81ba3ebd3ae74f9c0f90c63507ac89
GeekAn <geekan@foxmail.com>
2014-01-01 15:54:49 +0800

    update for registration

--- server/d1/social_auth_app/views.py
+++ server/d1/social_auth_app/views.py
@@ -1,16 +1,24 @@
+#coding:utf-8
+
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
 
+from django.http import HttpResponse
+
 from social.backends.google import GooglePlusAuth
 
+# XXX 如果oauth redirect此处失效
+# 下面的oauth认证过程，都采用了HTTP_REFERER作为返回redirect目标
+# 如果失效，应该考虑采用session方式，存储浏览器最后访问的页面
+# 具体的可以看看django的session方法
 
 def logout(request):
     """Logs out user"""
     auth_logout(request)
-    return redirect('/comment')
+    return redirect(request.META.get('HTTP_REFERER', '/comment'))
     #return render_to_response('home.html', {}, RequestContext(request))
 
 def home(request):
@@ -21,12 +29,14 @@ def home(request):
         'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None)
     }, RequestContext(request))
 
-
 @login_required
 def done(request):
     """Login complete view, displays user data"""
     scope = ' '.join(GooglePlusAuth.DEFAULT_SCOPE)
-    return redirect('/comment')
+    
+    print request
+    #return render(request, 'refresh_iframe.html')
+    return redirect(request.META.get('HTTP_REFERER', '/comment'))
     return render_to_response('done.html', {
         'user': request.user,
         'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None),
