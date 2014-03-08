12d3241ed636c5b69175ebf29db02e8a77eb29c2
geekben <bn0418@gmail.com>
2014-03-08 22:46:13 +0800

    make STATIC_URL local usable

--- server/d1/d1/settings.py
+++ server/d1/d1/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_NAME = '/static/'
-STATIC_URL = 'http://www.anwcl.com:8000' + STATIC_NAME
+STATIC_URL = STATIC_NAME
 
 # Additional locations of static files
 STATICFILES_DIRS = (
