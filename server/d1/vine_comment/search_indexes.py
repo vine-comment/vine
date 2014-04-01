b6fb62d6dda7e2c27d121872742810f4b1b465c4
GeekAn <geekan@foxmail.com>
2014-04-01 20:54:56 +0800

    search_index: init

---
+++ server/d1/vine_comment/search_indexes.py
@@ -0,0 +1,16 @@
+import datetime
+from haystack import indexes
+from vine_comment.models import Comment
+
+
+class CommentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='user')
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Comment
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
