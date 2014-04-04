9708635f4ad7c0d7a0723d62fe8242f76aa06e08
GeekAn <geekan@foxmail.com>
2014-04-04 22:06:37 +0800

    manage: add startup code for init jieba

--- server/d1/manage.py
+++ server/d1/manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import jieba
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "d1.settings")
 
     from django.core.management import execute_from_command_line
 
+    jieba.initialize()
     execute_from_command_line(sys.argv)
