a0c79227a3d0228a94e1ffeb463066784c20153f
GeekAn <geekan@foxmail.com>
2014-05-10 20:41:45 +0800

    prebuild.sh: implement pip section

--- prebuild.sh
+++ prebuild.sh
@@ -1,8 +1,27 @@
+#!/bin/sh
+
+##############################
+# Distribute Script for Vine #
+##############################
 
 # WARNING: the script is not done.
+# Read https://github.com/vine-comment/vine/blob/master/PREREQUISITE.md for more details.
+
+###############
+# pip section #
+###############
+
+items_pip=( django-registration django-crispy-forms django-admin-bootstrapped django-haystck jieba Whoosh Pillow python-social-auth python-memcached django_akismet_comments elasticsearch pyelasticsearch django-avatar )
 
 alias p='pip install -i http://pypi.douban.com/simple'
 
-p django-avatar
-p pillow
+for i in ${items_pip[@]}; do
+p $i
+done
+
+
+# TODO:
+# django-nonrel section
+# apt-get / download & setup section
+
 
