c7e40dbaddb5c0595177a18e1ec266fc4141f69b
Ben <bn0418@gmail.com>
2014-05-13 19:27:59 +0800

    Need to get root permission

--- prerequisite.sh
+++ prerequisite.sh
@@ -7,26 +7,23 @@
 # WARNING: the script is not done.
 # Read https://github.com/vine-comment/vine/blob/master/PREREQUISITE.md for more details.
 
-sudo -s
 
 ###############
 # pip section #
 ###############
 
-pip install -U pip -i http://pypi.douban.com/simple
+sudo pip install -U pip -i http://pypi.douban.com/simple
 
 if [ $? -ne 0 ]; then
-  apt-get install python-pip
+  sudo apt-get install python-pip
 fi
 
 items_pip=( django-registration django-crispy-forms django-admin-bootstrapped django-haystack jieba Whoosh Pillow python-social-auth python-memcached django_akismet_comments elasticsearch pyelasticsearch django-avatar )
 
-alias p='pip install -i http://pypi.douban.com/simple'
-
-
+alias p='sudo pip install -i http://pypi.douban.com/simple'
 
 for i in ${items_pip[@]}; do
-p $i
+  p $i
 done
 
 
