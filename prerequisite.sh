a2e143a523dc792257803973070dc0b70f95111d
Ben <bn0418@gmail.com>
2014-05-16 00:13:28 +0800

    Update prerequisite.sh

--- prerequisite.sh
+++ prerequisite.sh
@@ -37,6 +37,7 @@ source vine/bin/activate
 pip install git+https://github.com/django-nonrel/django@nonrel-1.5
 pip install git+https://github.com/django-nonrel/djangotoolbox
 pip install git+https://github.com/django-nonrel/mongodb-engine
+cd vine
 
 
 # TODO:
