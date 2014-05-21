f71b6b8d58782c7eb8693a5c3aee10e997d5f2d9
Ben <bn0418@gmail.com>
2014-05-21 20:32:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # Install Script for Vine #
 ##############################
 
+HOME=~
 # WARNING: the script is not done.
 # Read https://github.com/vine-comment/vine/blob/master/PREREQUISITE.md for more details.
 
@@ -15,8 +16,23 @@ sh prerequisite.sh
 pip install gunicorn
 cp gunicorn_start bin/.
 
+######################
+# supervisor section #
+######################
+sudo apt-get install supervisor
 
-# TODO:
-# apt-get / download & setup section
+(
+sudo cat <<EOF
+[program:vine]
+command = $HOME/vine/bin/gunicorn_start                                   ; Command to start app
+user = vine                                                               ; User to run as
+stdout_logfile = $HOME/vine/logs/gunicorn_supervisor.log                  ; Where to write log messages
+redirect_stderr = true 
+EOF
+) >/etc/supervisor/conf.d/.conf
 
+mkdir -p /logs
+touch $HOME/vine/logs/gunicorn_supervisor.log
 
+sudo supervisorctl reread
+sudo supervisorctl update
