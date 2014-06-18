e4531264d21e9729080777398f1131c8de834df5
geekan <geekan@foxmail.com>
2014-06-18 22:48:47 +0800

    update restart.sh

--- server/d1/restart.sh
+++ server/d1/restart.sh
@@ -1,6 +1,6 @@
 # NOTE: This script will kill all django processes.
-pkill -f 'python manage.py runserver 0.0.0.0:8000'
-pkill -f 'python -m smtpd -n -c DebuggingServer localhost:1025'
+pkill -i -f 'python manage.py runserver 0.0.0.0:8000'
+pkill -i -f 'python -m smtpd -n -c DebuggingServer localhost:1025'
 export PYTHONIOENCODING=utf-8
 python -m smtpd -n -c DebuggingServer localhost:1025 &
 python manage.py runserver 0.0.0.0:8000
