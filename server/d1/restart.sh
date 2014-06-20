a0bf53efbb2b2cfd007646b999e53abbf140fa18
geekan <geekan@foxmail.com>
2014-06-20 23:14:10 +0800

    restart.sh: update.. use sudo to make it work on mac

--- server/d1/restart.sh
+++ server/d1/restart.sh
@@ -1,6 +1,6 @@
 # NOTE: This script will kill all django processes.
-pkill -i -f 'python manage.py runserver 0.0.0.0:8000'
-pkill -i -f 'python -m smtpd -n -c DebuggingServer localhost:1025'
+sudo pkill -i -f 'python manage.py runserver 0.0.0.0:8000'
+sudo pkill -i -f 'python -m smtpd -n -c DebuggingServer localhost:1025'
 export PYTHONIOENCODING=utf-8
 python -m smtpd -n -c DebuggingServer localhost:1025 &
 python manage.py runserver 0.0.0.0:8000
