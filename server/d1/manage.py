3ba41be3fd889a035494446b41b8f9fb906cd8e2
geekan <geekan@foxmail.com>
2014-06-21 20:04:39 +0800

    fix run twice problem

--- server/d1/manage.py
+++ server/d1/manage.py
@@ -2,13 +2,15 @@
 import os
 import sys
 import jieba
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "d1.settings")
 
-    from django.core.management import execute_from_command_line
-
-    if 'runserver' in sys.argv:
-        if not jieba.initialized:
-            jieba.initialize()
+    # Autoreloader will take a reload, so check it or it will run twice:
+    # https://code.djangoproject.com/ticket/8085
+    if os.environ.get('RUN_MAIN', False):
+        if 'runserver' in sys.argv:
+            if not jieba.initialized:
+                jieba.initialize()
     execute_from_command_line(sys.argv)
