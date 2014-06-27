7c196e20482d33be5daab583e6fe4a1672326dc9
geekan <geekan@foxmail.com>
2014-06-27 14:40:24 +0800

    update checkenv.sh

--- server/d1/checkenv.sh
+++ server/d1/checkenv.sh
@@ -1,3 +1,6 @@
+
+echo 'Checking Elasticsearch and mongodb processes..'
+
 ps aux | grep '[e]lasticsearch' && echo 'Elasticsearch is running.'
 ps aux | grep '[m]ongod' && echo 'Mongodb is running.'
 
