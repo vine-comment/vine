8044f4c513e6153ad1829261a6a22bea92eb5a47
geekben <bn0418@gmail.com>
2014-08-12 22:04:45 +0800

    Enable chrome extension for current home website

--- browser/user-extension/chrome-extension/god-alike-comment/shentucao.js
+++ browser/user-extension/chrome-extension/god-alike-comment/shentucao.js
@@ -1,3 +1,15 @@
+// Load the script
+var script = document.createElement("SCRIPT");
+script.src = 'http://code.jquery.com/jquery-1.8.2.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName("head")[0].appendChild(script);
+
+script.src = 'http://www.anwcl.com/static/js/btoa.js';
+script.type = 'text/javascript';
+document.getElementsByTagName("head")[0].appendChild(script);
+
+if (!window.btoa) window.btoa = base64.encode;
 var jQuery_anwcl = jQuery.noConflict();
 jQuery_anwcl('body').append('<div id="test"></div>');
-jQuery_anwcl('#test').load("http://www.anwcl.com:8000/static/test/iframe_test.html");
+jQuery_anwcl('#test').load("http://www.anwcl.com/iframe/" + btoa(document.location));
+
