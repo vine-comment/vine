e580730e609c2e1dc01f311ae54b4bcd13dba9f9
geekan <geekan@foxmail.com>
2014-10-19 21:40:56 +0800

    update url_tag_client

--- server/d1/vine_comment/url_tag_client.py
+++ server/d1/vine_comment/url_tag_client.py
@@ -1,19 +1,33 @@
+#!/usr/bin/env python
+#coding:utf8
 
+import argparse
 
+parser = argparse.ArgumentParser(description='Test parser.')
+parser.add_argument('--add', help='add url tag combination')
+parser.add_argument('--rem', help='rem url tag combination')
+parser.add_argument('--get', help='get url tag combination')
+parser.add_argument('--mod', help='mod url tag combination')
 
-class UrlTag():
+class UrlTag(object):
     def add():
+        print('add')
         pass
-    def del():
+    def rem():
+        print('rem')
         pass
     def get():
+        print('get')
         pass
     def mod():
+        print('mod')
         pass
 
+def main():
+    pass
 
-
-
+if __name__ == '__main__':
+    main()
 
 
 
