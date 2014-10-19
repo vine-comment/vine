a796a938f61c64c9851384c5dd3db295161b8567
geekan <geekan@foxmail.com>
2014-10-19 21:51:28 +0800

    update

--- server/d1/vine_comment/url_tag_client.py
+++ server/d1/vine_comment/url_tag_client.py
@@ -3,11 +3,11 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Url-Tag-Combination Client.')
-parser.add_argument('--add', help='add url tag combination')
-parser.add_argument('--rem', help='rem url tag combination')
-parser.add_argument('--get', help='get url tag combination')
-parser.add_argument('--mod', help='mod url tag combination')
+parser = argparse.ArgumentParser(description='Url Tag Combination Client.')
+parser.add_argument('-a', '--add', help='add url tag combination', nargs=2, metavar=('url', 'tag'))
+parser.add_argument('-r', '--rem', help='rem url tag combination', nargs=2, metavar=('url', 'tag'))
+parser.add_argument('-g', '--get', help='get url tag combination', nargs=2, metavar=('url', 'tag'))
+parser.add_argument('-m', '--mod', help='mod url tag combination', nargs=2, metavar=('url', 'tag'))
 
 class UrlTag(object):
     def add():
