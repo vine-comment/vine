b8f6a13ddbf61a98ad7fd31c569ca26818c7d0fd
geekan <geekan@foxmail.com>
2014-12-22 22:55:24 +0800

    restart.sh: add check mongodb logic

--- server/d1/restart.sh
+++ server/d1/restart.sh
@@ -6,6 +6,18 @@ pkill -f 'Python -m smtpd -n -c DebuggingServer localhost:1025'
 pkill -f 'python manage.py runserver 0.0.0.0:8000'
 pkill -f 'python -m smtpd -n -c DebuggingServer localhost:1025'
 
+echo "\ncheck utils running...\n"
+
+mongodb=$(ps aux | grep -E "[m]ongo")
+if [ -z "$mongodb" ]; then
+    echo "mongodb is not running! abort!"
+    exit -1
+else
+    echo "mongodb is running!"
+fi
+
+echo "\ncheck utils done...\n"
+
 export PYTHONIOENCODING=utf-8
 python -m smtpd -n -c DebuggingServer localhost:1025 &
 python manage.py runserver 0.0.0.0:8000
