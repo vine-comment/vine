4fec66c8b7a90e5f15687c2925f1685f01ac002b
geekan <geekan@foxmail.com>
2014-12-22 11:45:43 +0800

    use vine-comment

---
+++ browser/user-extension/chrome-extension/vine-comment/comment_updown.js
@@ -0,0 +1,74 @@
+		function up(id) {
+            var onclick = $('#up_comment_'+id).attr('onclick');
+            $('#up_comment_'+id).prop('onclick','return false;');
+		    $.get("/ajax/up/comment/"+id)
+			 .done(function(data){
+					if (data == "up-1") {
+							popup_message(data);
+							var ups = $('#up_comment_'+id).find('span');
+							var count = parseInt(ups.text())-1;
+							ups.text(count);
+							var up_icon = $('#up_comment_'+id).find('i');
+							up_icon.prop('class', "fa fa-thumbs-o-up");
+					} else if (data == "up+1") {
+							popup_message(data);
+							var ups = $('#up_comment_'+id).find('span');
+							var count = parseInt(ups.text())+1;
+							ups.text(count);
+							var up_icon = $('#up_comment_'+id).find('i');
+							up_icon.prop('class', "fa fa-thumbs-up");
+					} else if (data == "down-1,up+1") {
+							popup_message(data);
+							var downs = $('#down_comment_'+id).find('span');
+							var count = parseInt(downs.text())-1;
+							downs.text(count);
+							var down_icon = $('#down_comment_'+id).find('i');
+							down_icon.prop('class', "fa fa-thumbs-o-down fa-flip-horizontal");
+
+							var ups = $('#up_comment_'+id).find('span');
+							var count = parseInt(ups.text())+1;
+							ups.text(count);
+							var up_icon = $('#up_comment_'+id).find('i');
+							up_icon.prop('class', "fa fa-thumbs-up");
+					} else if (data == "non-user") {
+							popup_message("亲，注册个用户吧");
+					}
+                    $('#up_comment_'+id).attr('onclick',onclick);//must use attr, not prop
+			 });
+		};
+		function down(id) {
+            var onclick = $('#down_comment_'+id).attr('onclick');
+            $('#down_comment_'+id).prop('onclick','return false;');
+			$.get("/ajax/down/comment/"+id)
+			 .done(function(data){
+					if (data == "down-1") {
+							popup_message(data);
+							var downs = $('#down_comment_'+id).find('span');
+							var count = parseInt(downs.text())-1;
+							downs.text(count);
+							var down_icon = $('#down_comment_'+id).find('i');
+							down_icon.prop('class', "fa fa-thumbs-o-down fa-flip-horizontal");
+					} else if (data == "down+1") {
+							popup_message(data);
+							var downs = $('#down_comment_'+id).find('span');
+							var count = parseInt(downs.text())+1;
+							downs.text(count);
+							var down_icon = $('#down_comment_'+id).find('i');
+							down_icon.prop('class', "fa fa-thumbs-down fa-flip-horizontal");
+					} else if (data == "up-1,down+1") {
+							popup_message(data);
+							var ups = $('#up_comment_'+id).find('span');
+							var count = parseInt(ups.text())-1;
+							ups.text(count);
+							var up_icon = $('#up_comment_'+id).find('i');
+							up_icon.prop('class', "fa fa-thumbs-o-up");
+
+							var downs = $('#down_comment_'+id).find('span');
+							var count = parseInt(downs.text())+1;
+							downs.text(count);
+							var down_icon = $('#down_comment_'+id).find('i');
+							down_icon.prop('class', "fa fa-thumbs-down fa-flip-horizontal");
+					}
+                    $('#down_comment_'+id).attr('onclick',onclick);//must use attr, not prop
+			 });
+		};
---
+++ browser/user-extension/chrome-extension/vine-comment/popup.js
@@ -0,0 +1,17 @@
+
+/*
+document.addEventListener('DOMContentLoaded', function () {
+	if (!window.btoa) window.btoa = base64.encode;
+	$('body').append('<div id="vine_comment_iframe"></div>');
+	$.support.cors = true;
+	$('#vine_comment_iframe').load("http://www.anwcl.com/comment_raw/" + btoa(document.location));
+});
+*/
+
+chrome.tabs.getSelected(null,function(tab) {
+    var tablink = tab.url;
+	if (!window.btoa) window.btoa = base64.encode;
+	//$('body').append('<div id="vine_comment_iframe">Loading...</div>');
+	$.support.cors = true;
+	$('body iframe').attr('src', "http://www.tengmanpinglun.com/comment/" + btoa(tablink));
+});
