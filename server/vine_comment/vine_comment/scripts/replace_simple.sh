354b8942429ee3ee150f3f38e5d52b4371523168
geekan <geekan@foxmail.com>
2015-01-01 23:55:04 +0800

    rename 1.sh and 2.sh, and move them to scripts

---
+++ server/vine_comment/vine_comment/scripts/replace_simple.sh
@@ -0,0 +1,6 @@
+
+alias gf='find . -type f | xargs grep -nE --color'
+
+i="\'comments\/"
+replace="\'plugin\/comments\/"
+gf -l $i | grep -vE "(xxx|Binary|json|pyc$)" | xargs sed -i '' "s/$i/$replace/g"
---
+++ server/vine_comment/vine_comment/scripts/replace_with_xxx.sh
@@ -0,0 +1,10 @@
+xxx=`cat xxx`
+
+alias gf='find . -type f | xargs grep -nE --color'
+for i in $xxx; do
+    echo $i
+    echo 'plugin\/'$i
+    replace='plugin\/'$i
+    gf -l $i | grep -vE "(xxx|Binary|json|pyc$|html$)" | xargs sed -i '' "s/$i/$replace/g"
+    # sedr $i 'plugin\/'$i
+done
