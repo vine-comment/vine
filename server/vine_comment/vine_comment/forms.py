89436a1b180e2633039ef4d9b02c3be5c3626b7e
geekan <geekan@foxmail.com>
2015-01-06 02:01:40 +0800

    forms: localize registration error msg

--- server/vine_comment/vine_comment/forms.py
+++ server/vine_comment/vine_comment/forms.py
@@ -22,7 +22,7 @@ class VineRegistrationForm(forms.Form):
     username = forms.RegexField(regex=r'^[\w.@+-]+$',
                                 max_length=30,
                                 label="用户名",
-                                error_messages={'invalid': "This value may contain only letters, numbers and @/./+/-/_ characters."})
+                                error_messages={'invalid': "只能包含数字、字母、与@/./+/-/_等符号。"})
     email = forms.EmailField(label="邮箱")
     password1 = forms.CharField(widget=forms.PasswordInput,
                                 label="密码")
@@ -36,14 +36,14 @@ class VineRegistrationForm(forms.Form):
     def clean_username(self):
         existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
         if existing.exists():
-            raise forms.ValidationError("A user with that username already exists.")
+            raise forms.ValidationError("用户名已存在。")
         else:
             return self.cleaned_data['username']
 
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError("The two password fields didn't match.")
+                raise forms.ValidationError("两次输入的密码不一致。")
         return self.cleaned_data
     def clean_email(self):
         """
@@ -51,7 +51,7 @@ class VineRegistrationForm(forms.Form):
         site.
         """
         if User.objects.filter(email__iexact=self.cleaned_data['email']):
-            raise forms.ValidationError(_("This email address is already in use. Please supply a different email address."))
+            raise forms.ValidationError(_("此email地址已被使用，请使用不同的email地址。"))
         return self.cleaned_data['email']
 
 
