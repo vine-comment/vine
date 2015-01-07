b37eb5952dc07b2e40a5b6e78177a4c2561af210
geekan <geekan@foxmail.com>
2015-01-07 23:40:57 +0800

    registration: fix simple reg

--- server/vine_comment/vine_comment/urls.py
+++ server/vine_comment/vine_comment/urls.py
@@ -14,7 +14,7 @@ from django.contrib.auth.decorators import login_required
 
 from registration.forms import RegistrationFormTermsOfService
 from registration.backends.views import RegistrationView
-from registration.backends.views.simple import RegistrationView as RegistrationSimpleView
+from registration.backends.simple.views import RegistrationView as RegistrationSimpleView
 
 
 # NOT USE NOW.
