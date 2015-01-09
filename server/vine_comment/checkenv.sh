fb0b90f5f98810872c44ec1a1afaacaae0e8c23f
geekan <geekan@foxmail.com>
2015-01-09 22:55:12 +0800

    checkenv: add redis check

--- server/vine_comment/checkenv.sh
+++ server/vine_comment/checkenv.sh
@@ -1,6 +1,9 @@
 
-echo 'Checking Elasticsearch and mongodb processes..'
+echo ''
+echo 'Checking Elasticsearch / mongodb / redis processes..'
+echo ''
 
 ps aux | grep '[e]lasticsearch' && echo 'Elasticsearch is running.'
 ps aux | grep '[m]ongod' && echo 'Mongodb is running.'
+ps aux | grep '[r]edis' && echo 'Redis is running.'
 
