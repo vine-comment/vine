15a8cf5ef7200d2121852de962759d9c02d889b7
geekan <geekan@foxmail.com>
2015-01-10 17:28:10 +0800

    tampermonkey-script: update miniscript to use it at baidu.com

--- browser/user-extension/tampermonkey-script/mini_script.js
+++ browser/user-extension/tampermonkey-script/mini_script.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name       try-to-comment
-// @namespace  http://www.anwcl.com/
+// @namespace  http://www.baidu.com/
 // @version    0.2
 // @description  3rd party comment plugin
-// @match      http://www.anwcl.com/contact.html
-// @exclude    http://www.anwcl.com:8000/*
+// @match      http://www.baidu.com/*
 // @copyright  2012+, You
 // @require    http://code.jquery.com/jquery-1.8.2.min.js
+// @require    http://www.tengmanpinglun.com/static/js/btoa.js
 // @grant      unsafeWindow
 // ==/UserScript==
 
+window.btoa = base64.encode;
 $('body').append('<div id="vine_comment_iframe"></div>');
-$('#vine_comment_iframe').load("http://www.anwcl.com:8000/iframe/" + btoa(document.location));
+$('#vine_comment_iframe').load("http://www.tengmangpinglun.com/iframe/" + btoa(document.location));
