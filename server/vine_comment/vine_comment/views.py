d26240e0477f2de5c8db2bef117e13850d95e407
geekan <geekan@foxmail.com>
2015-03-15 15:45:48 +0800

    implement document uplaod

--- server/vine_comment/vine_comment/views.py
+++ server/vine_comment/vine_comment/views.py
@@ -1169,7 +1169,22 @@ class MostActiveUsersView(TemplateView):
 class DocumentUploadView(TemplateView):
 
     def get(self, request, *args, **kwargs):
-        pass
+        # Load documents for the list page
+        documents = Document.objects.all()
+
+        # Render list page with the documents and the form
+        return render_to_response(
+            'myapp/list.html',
+            {'documents': documents, 'form': form},
+            context_instance=RequestContext(request)
+        )
 
     def post(self, request, *args, **kwargs):
-        pass
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect('/')
+
